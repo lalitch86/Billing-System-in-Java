16a2010f1c44c205970427f7ebb5fc56a5bbfc57
unknown <lalitchaudhari1437@gmail.com>
2024-01-31 00:02:56 +0530

    first commits

---
+++ src/bms/Bms.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bms;
+
+/**
+ *
+ * @author Admin
+ */
+public class Bms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
